23f183f4fcf35be2e267918fdec854abdecb0d77
afanimartin <afani.dev@gmail.com>
2024-05-31 15:07:47 +0200

    Add env host config for prod and dev environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.hosts << "rails-production-21c5.up.railway.app"
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
